5dcbdbb9f6ca9ed3c8f35d5b535023823a190cf1
Amy Hsieh <hahsieh@outlook.com>
2019-11-09 15:55:05 -0800

    initial version

---
+++ HashingWithStringKeys/P260HashTable.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HashingWithStringKeys
+{
+    public class KeyAndValue
+    {
+        public string Key { get; internal set; }
+        public string Value { get; set; }
+        public KeyAndValue(string key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+    }
+    public class P260HashTable
+    {
+        // hash TABLE is size pSize, stores data in aarray that goes from 0 to pSize-1
+        internal LinkedList<KeyAndValue>[] hashTable;  // Relying on new LinkedList is initialized with all values = null
+        int tableSize;
+        int collisions = 0;
+
+
+        // constructor --- user specifies how big the table they want to use
+        public P260HashTable(int pSize)
+        {
+            hashTable = new LinkedList<KeyAndValue>[pSize];
+            tableSize = pSize;
+        }
+
+        public bool AddItem(string key, string value)
+        {
+            int hashIndex = Hash(key, tableSize);
+            KeyAndValue kvp;
+
+            Console.Write($"Key {key} hashes to {hashIndex,2}.  ");
+            if (hashTable[hashIndex] == null)  // null value means this slot is empty, so we can write our data (now a string) here.
+            {
+                hashTable[hashIndex] = new LinkedList<KeyAndValue>();                
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.DarkRed;
+                Console.WriteLine($"<<< COLLISION! But we'll take care of it."); // else this spot was used
+                Console.ResetColor();
+                collisions++;                
+            }
+            kvp = new KeyAndValue(key, value);
+            hashTable[hashIndex].AddLast(kvp);
+            Console.WriteLine($"key {key}, Value {value} stored.");
+            return true;
+        }
+
+        public KeyAndValue GetItem(string key)  
+        {
+            int hashIndex = Hash(key, tableSize);
+            LinkedListNode<KeyAndValue> node;
+            if (hashTable[hashIndex] == null)  // null value means this slot is empty
+            {
+                return null;
+            }
+            node = hashTable[hashIndex].First;
+            while (node != null && node.Value.Key != key)
+            {
+                node = node.Next;
+            }
+            if (node == null)
+            {
+                return null;
+            }
+            return node.Value;
+        }
+
+        public bool DeleteItem(string key)
+        {
+            int hashIndex = Hash(key, tableSize);
+            LinkedListNode<KeyAndValue> node;
+            if (hashTable[hashIndex] == null)
+            {
+                return false;
+            }
+            node = hashTable[hashIndex].First;
+            while (node != null && node.Value.Key!=key)
+            {
+                node = node.Next;
+            }
+            if (node.Value.Key == key)
+            {
+                hashTable[hashIndex].Remove(node);
+                if (hashTable[hashIndex].Count >= 1)
+                {
+                    collisions--;
+                }
+                else if (hashTable[hashIndex].Count == 0)
+                {
+                    hashTable[hashIndex] = null;
+                }
+                else
+                {
+                    // Throw if non-negative assumption on count doesn't hold.
+                    throw new Exception("Count must be positive or zero.");
+                }
+                return true;
+            }
+            return false;            
+        }
+
+
+        public KeyAndValue UpdateItem(string key, string value)
+        {
+            int hashIndex = Hash(key, tableSize);
+            LinkedListNode<KeyAndValue> node;
+            if (hashTable[hashIndex] == null)
+            {
+                return null;
+            }
+            node = hashTable[hashIndex].First;
+            while (node != null && node.Value.Key != key)
+            {
+                node = node.Next;
+            }
+            if (node.Value.Key == key)
+            {
+                node.Value.Value = value;
+                return node.Value;
+            }
+            return null;            
+        }
+
+        static public int Hash(string key, int numSlots)
+        {
+            double count = 0;
+            double hashIndex = 0;
+            double radix = 128;
+
+            foreach (char a in key)
+            {
+                if (count == 4) count = 0;
+                hashIndex += (double)a * (Math.Pow((double)radix, count));
+                count++;
+            }
+
+            return (int)(hashIndex % numSlots);
+        }
+
+        public void PrintTableState()
+        {
+            LinkedListNode<KeyAndValue> node;
+            for (int i = 0; i < hashTable.Length; i++)
+            {
+                if (hashTable[i] == null)
+                {
+                    Console.WriteLine($"[{i,2}]\t= <<empty>>");
+                }
+                else
+                {
+                    node = hashTable[i].First;
+                    for (int j = 0; j < hashTable[i].Count; j++)
+                    {
+                        Console.WriteLine($"[{i,2}] \t= {node.Value.Key}: {node.Value.Value}");
+                        node = node.Next;
+                    }                    
+                }
+            }
+            Console.WriteLine("\nTotal number of collisions: " + collisions);
+        }
+    }
+
+}
---
+++ HashingWithStringKeys/Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace HashingWithStringKeys
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("Enter the number of elements your hash table will hold: ");
+            int.TryParse(ReadLine(), out int numberOfElements);
+            if(numberOfElements == 0)
+            {
+                WriteLine("Invalid value.  Defaulting to 30.");
+                numberOfElements = 30;
+            }
+            P260HashTable teams = new P260HashTable(numberOfElements);
+            string[] teamNames = LoadTeamTable();
+            string[] teamLocations = LoadTeamLocation();
+            KeyAndValue team;
+
+            WriteLine();
+            //Add each entry from the parallel team name and location arrays to the dictionary
+            WriteLine("Initial load of hash table");
+            WriteLine("--------------------------");
+            for (int i = 0; i < teamNames.Length; i++)
+            {
+                teams.AddItem(teamNames[i], teamLocations[i]);
+            }
+
+            //CRUD operations on items
+            do
+            {
+                string teamName;
+                string teamLocation;
+
+
+                //Print option is for debug/demonstration purposes only
+                Write("\n(C)reate, (R)ead, (U)pdate, (D)elete team, or (P)rint hash table state? => ");
+                switch (ReadLine().ToUpper().Substring(0, 1))
+                {
+                    case "C":
+                        Write("Enter new team name: ");
+                        teamName = ReadLine();
+                        Write("Enter team location: ");
+                        teamLocation = ReadLine();
+                        if (teams.AddItem(teamName, teamLocation))
+                        {
+                            WriteLine($"\nThe {teamLocation} {teamName} are now part of the NFL!");
+                        }
+                        else
+                        {
+                            WriteLine($"\n{teamLocation} {teamName} couldn't be added.  Bummer.");
+                        }
+                        break;
+                    case "R":
+                        Write("Enter team name to retrieve its location: ");
+                        teamName = ReadLine();
+                        team = teams.GetItem(teamName);
+                        if (team != null)
+                        {
+                            WriteLine($"\nThe {team.Key} are in {team.Value}, at least for now.");
+                        }
+                        else
+                        {
+                            WriteLine($"Team \"{teamName}\" doesn't exist.");
+                        }
+                        break;
+                    case "U":
+                        Write("Enter team name to update: ");
+                        teamName = ReadLine();
+
+                        team = teams.GetItem(teamName);
+                        if (team != null)
+                        {
+                            Write($"\nCurrent location is {team.Value}.  Where do you want to move them? ");
+                            team = teams.UpdateItem(teamName, ReadLine());
+                            WriteLine($"Okey doke.  The {team.Key} are now in {team.Value}.");
+                        }
+                        else
+                        {
+                            WriteLine($"Team \"{teamName}\" doesn't exist.");
+                        }
+                        break;
+                    case "D":
+                        Write("Enter team to delete: ");
+                        teamName = ReadLine();
+
+                        team = teams.GetItem(teamName);
+                        if (team != null)
+                        {
+                            if (teams.DeleteItem(teamName))
+                            {
+                                WriteLine($"Okey doke.  The {teamName} are history.");
+                            }
+                            else
+                            {
+                                WriteLine($"Oops.  Something went wrong.");
+                            }
+                        }
+                        else
+                        {
+                            WriteLine($"Team \"{teamName}\" doesn't exist.");
+                        }
+                        break;
+                    case "P":
+                        WriteLine("\nCurrent hash table state: ");
+                        teams.PrintTableState();
+                        break;
+                    default:
+                        WriteLine("Invalid command.");
+                        break;
+                }
+                Write("\nContinue? ");
+
+
+            } while (ReadLine().ToUpper().Substring(0, 1) != "N");
+
+            //do
+            //{
+            //    string teamName;
+            //    string teamLocation;
+
+            //    WriteLine("\nInitial hash table state: ");
+            //    teams.PrintTableState();
+
+            //    Write("\n(C)reate, (R)ead, (U)pdate, or (D)elete team? => ");
+            //    switch (ReadLine().ToUpper().Substring(0, 1))
+            //    {
+            //        case "C":
+            //            Write("Enter new team name: ");
+            //            teamName = ReadLine();
+            //            Write("Enter team location: ");
+            //            teamLocation = ReadLine();
+            //            team = new KeyAndValue(teamName, teamLocation);
+            //            if(teams.AddItem(team))
+            //            {
+            //                WriteLine($"\nThe {teamLocation} {teamName} are now part of the NFL!");
+            //            }
+            //            else
+            //            {
+            //                WriteLine($"\n{teamLocation} {teamName} couldn't be added.  Bummer.");
+            //            }
+            //            break;
+            //        case "R":
+            //            Write("Enter team name to retrieve its location: ");
+            //            teamName = ReadLine();
+            //            team = teams.GetItem(teamName);
+            //            if (team != null)
+            //            {
+            //                WriteLine($"\nThe {team.Key} are in {team.Value}, at least for now.");
+            //            }
+            //            else
+            //            {
+            //                WriteLine($"Team \"{teamName}\" doesn't exist.");
+            //            }
+            //            break;
+            //        case "U":
+            //            Write("Enter team name to update: ");
+            //            teamName = ReadLine();
+
+            //            team = teams.GetItem(teamName);
+            //            if (team != null)
+            //            {
+            //                Write($"\nCurrent location is {team.Value}.  Where do you want to move them? ");
+            //                team = teams.UpdateItem(teamName, ReadLine());
+            //                WriteLine($"Okey doke.  The {team.Key} are now in {team.Value}.");
+            //            }
+            //            else
+            //            {
+            //                WriteLine($"Team \"{teamName}\" doesn't exist.");
+            //            }
+            //            break;
+            //        case "D":
+            //            Write("Enter team to delete: ");
+            //            teamName = ReadLine();
+
+            //            team = teams.GetItem(teamName);
+            //            if (team != null)
+            //            {
+            //                if (teams.DeleteItem(teamName))
+            //                {
+            //                    WriteLine($"Okey doke.  The {teamName} are history.");
+            //                }
+            //                else
+            //                {
+            //                    WriteLine($"Oops.  Something went wrong.");
+            //                }
+            //            }
+            //            else
+            //            {
+            //                WriteLine($"Team \"{teamName}\" doesn't exist.");
+            //            }
+            //            break;
+
+            //        default:
+            //            WriteLine("Invalid command.");
+            //            break;
+            //    }
+            //    WriteLine("\nCurrent hash table state: ");
+            //    teams.PrintTableState();
+            //    Write("\nContinue? ");
+
+
+            //} while (ReadLine().ToUpper().Substring(0, 1) != "N");
+            WriteLine();
+            WriteLine("Here's what we now have: ");
+
+
+            WriteLine("Press a key to stop the insanity.");
+            ReadKey();
+        }
+
+        private static string[] LoadTeamTable()
+        {
+            return new string[32] {"Cardinals","Falcons","Ravens","Bills",
+                "Panthers","Bears","Bengals","Browns",
+                "Cowboys","Broncos","Lions","Packers",
+                "Texans","Colts","Jaguars","Chiefs",
+                "Chargers","Rams","Dolphins","Vikings",
+                "Patriots","Saints","Giants","Jets",
+                "Raiders","Eagles","Steelers","49ers",
+                "Seahawks","Buccaneers","Titans","Redskins"};
+        }
+
+        private static string[] LoadTeamLocation()
+        {
+            return new string[32] {"Arizona","Atlanta","Baltimore","Buffalo",
+            "Carolina","Chicago","Cincinnati","Cleveland",
+            "Dallas","Denver","Detroit","Green Bay",
+            "Houston","Indianapolis","Jacksonville","Kansas City",
+            "Los Angeles","Los Angeles", "Miami","Minnesota",
+            "New England","New Orleans","New York","New York",
+            "Oakland","Philadelphia","Pittsburgh","San Francisco",
+            "Seattle","Tampa Bay","Tennessee", "Washington"};
+        }
+    }
+}
+
